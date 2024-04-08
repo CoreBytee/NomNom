fa82f04e2216acf76c8cdb105acc3011d7dfe11e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-08 12:48:45 +0200

    add roommanager

--- backend/Classes/Game.js
+++ backend/Classes/Game.js
@@ -1,11 +1,13 @@
 import Logger from "@corebyte/logger";
 import Server from "./Server";
+import RoomManager from "./RoomManager";
 
 export default class Game {
     constructor() {
         this.Logger = new Logger("Game")
         this.Logger.Information("Loading game...")
 
+        this.RoomManager = new RoomManager(this)
         this.Server = new Server(this)
     }
 }

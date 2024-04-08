dbf65ca796e894bf13365c458443fd628b9d0aed
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-08 12:49:18 +0200

    setup classes

--- frontend/assets/script/Classes/Game.js
+++ frontend/assets/script/Classes/Game.js
@@ -1,4 +1,27 @@
+import Background from "./Background.js"
+
 export default class Game {
     constructor() {
+        new p5(
+            (p) => {
+                p.setup = () => p.createCanvas(p.windowWidth, p.windowHeight),
+                p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight),
+                p.draw = () => this.Draw()
+                this.P5 = p
+            },
+            document.querySelector("body")
+        )
+        console.log(this.P5)
+
+        // Game State
+        this.State = "Menu"
+
+        // Objects
+        this.Background = new Background(this)
+    }
+
+    Draw() {
+        console.log("Drawing")
+        this.Background.Draw(this.P5)
     }
 }

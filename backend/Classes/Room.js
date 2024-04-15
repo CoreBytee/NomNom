7b15d80dd27c32701aedbcdc9b8bf4159debd788
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-15 14:15:46 +0200

    add ticking

--- backend/Classes/Room.js
+++ backend/Classes/Room.js
@@ -1,13 +1,39 @@
+import Player from "./Player"
+
 export default class Room {
-    
     constructor(Game, RoomId) {
         this.Game = Game
         this.RoomId = RoomId
         this.Players = []
 
+        this.TickLoop()
+    }
+
     get PlayerCount() {
         return this.Players.length
     }
+
+    TickLoop() {
+        let TickCompleted = true
+
+        setInterval(
+            () => {
+                if (!TickCompleted) {
+                    this.Game.Logger.Warning("Previous tick not completed, skipping")
+                    return
+                }
+                TickCompleted = false
+                this.Tick()
+                TickCompleted = true
+            },
+            50 // 20 ticks per second
+        )
+    }
+
+    Tick() {
+        
+    }
+
     ConnectPlayer(Connection) {
         const NewPlayer = new Player(Room, Connection)
         this.Players.push(NewPlayer)

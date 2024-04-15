fe26806cd8e72a91d6f748dac55d982021075e68
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-15 14:15:28 +0200

    spawn local player

--- frontend/assets/script/Classes/LocalPlayer.js
+++ frontend/assets/script/Classes/LocalPlayer.js
@@ -1,5 +1,38 @@
 export default class LocalPlayer {
-    constructor() {
+    constructor(Game) {
+        this.Game = Game
+        this.P5 = Game.P5
+
+        this.PositionX = 2500
+        this.PositionY = 2500
+    }
+
+    Setup() {
+
+    }
+
+    PreDraw() {
+
+    }
+
+    Draw() {
+        if (this.Game.State != "Game") { return }
+        this.P5.fill(255, 0, 0)
+        this.P5.stroke(0, 0, 0)
+        this.P5.circle(
+            this.PositionX,
+            this.PositionY,
+            50
+        )
+
+        this.Game.Camera.EasePosition(
+            this.PositionX,
+            this.PositionY,
+            10
+        )
+    }
+
+    AfterDraw() {
 
     }
 }

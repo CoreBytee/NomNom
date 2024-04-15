3157aa834b91fd1f5a4a68f6fb5565f8d75b0038
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-15 14:13:08 +0200

    find room for player and connect to it

--- backend/Classes/Room.js
+++ backend/Classes/Room.js
@@ -1,3 +1,15 @@
 export default class Room {
     
+    constructor(Game, RoomId) {
+        this.Game = Game
+        this.RoomId = RoomId
+        this.Players = []
+
+    get PlayerCount() {
+        return this.Players.length
+    }
+    ConnectPlayer(Connection) {
+        const NewPlayer = new Player(Room, Connection)
+        this.Players.push(NewPlayer)
+    }
 }
--- backend/Classes/RoomManager.js
+++ backend/Classes/RoomManager.js
@@ -1,3 +1,18 @@
 export default class RoomManager {
-    
+    constructor(Game) {
+        this.Game = Game
+        this.Rooms = []
+    }
+
+    async HandleConnection(Connection) {
+        let FoundRoom = this.Rooms.find(Room => Room.PlayerCount < 10)
+
+        if (!FoundRoom) {
+            FoundRoom = new Room(this.Game, this.Rooms.length)
+            this.Rooms.push(Room)
+        }
+
+        this.Game.Logger.Information("Player connected to room " + FoundRoom.RoomId)
+        FoundRoom.ConnectPlayer(Connection)
+    }
 }

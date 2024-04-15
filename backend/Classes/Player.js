bee257a1ae0e743176d21ec135ef897a8e72af2f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-15 14:13:25 +0200

    spawn player at random position

--- backend/Classes/Player.js
+++ backend/Classes/Player.js
@@ -1,3 +1,19 @@
+import random from "just-random-integer"
+
 export default class Player {
-    
+    constructor(Room, Connection) {
+        this.Room = Room
+        this.Connection = Connection
+
+        this.PositionX = random(100, 5000 - 100)
+        this.PositionY = random(100, 5000 - 100)
+
+        this.SendMessage(
+            "Spawn",
+            {
+                PositionX: this.PositionX,
+                PositionY: this.PositionY
+            }
+        )
+    }
 }
